bcbb9cc1962d05cd5eeac1972e50652a7237d012
GolanSabo <Golansabo@yahoo.com>
2016-07-16 16:52:39 +0300

    Test

--- TestWidgets/TextBoxTest.cpp
+++ TestWidgets/TextBoxTest.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "../Label/Label.h"
-#include "../Panel/Panel.h"
+//#include "../Panel/Panel.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
